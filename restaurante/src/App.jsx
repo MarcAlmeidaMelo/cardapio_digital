37b9a728c19dfefd845fdfac3337bf5691926a77
marcelmelo <almeida.marcel.melo@gmail.com>
2025-10-16 16:47:20 -0300

    ....

---
+++ restaurante/src/App.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import imgRestaurante from "./assets/hashtaurante.webp";
+
+function App() {
+  return <img src={imgRestaurante} alt="banner restaurante"></img>;
+}
+
+export default App;
